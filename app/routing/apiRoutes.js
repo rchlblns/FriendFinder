759a08d03d3aec6d79ed0ba29c60030e7160748d
Short Stack <41342213+rchlblns@users.noreply.github.com>
2018-11-09 18:37:06 -0600

    get and post routes added to  apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,20 @@
-app.get("/api/friends", function(req,res){
-    
-});
+const friends = require("../data/friends.json");
 
-app.post("api/friends", function(req,res){
-    var newFriend = req.body;
+module.exports = function(app){
+    app.get("/api/friends", function(req,res){
+        return res.json(friends);
+    });
 
-})
+    app.post("/api/friends", function(req,res){
+        
+        const newFriend = req.body;
+
+        newFriend.routeName = newFriend.name.replace(/\s+/g,"").toLowerCase();
+
+        console.log(newFriend);
+
+        friends.push(newFriend);
+
+    });
+}
+
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 let htmlRoute  = require("./app/routing/htmlRoutes");
 require("./app/routing/htmlRoutes")(app);
 
+let apiRoute = require("./app/routing/apiRoutes");
+require("./app/routing/apiRoutes")(app);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function(){
